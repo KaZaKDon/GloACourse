266aaf502ece41571ec6c885f662005a7db847bc
Kazak <strannik19742@mail.ru>
2025-07-22 21:04:55 +0300

    lesson01

--- Lesson01/script.js
+++ Lesson01/script.js
@@ -1,2 +1,9 @@
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
+
 alert("Начало обучения");
 console.log("первое задание");
