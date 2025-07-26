9a5b3290899df06cde531598eef966b7b42be2f9
Kazak <strannik19742@mail.ru>
2025-07-26 13:03:27 +0300

    lesson03

---
+++ lesson03/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const title = prompt("Как называется Ваш проект?");
+const screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let rollback = 10;
+let fullPrice = 0;
+let servicePercentPrice = 0;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+
+console.log(fullPrice);
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log(
+      `Даем скидку 10%. Итоговая сумма: ${
+        servicePercentPrice - (servicePercentPrice * 10) / 100
+      } рублей`
+    );
+    break;
+  case 15000 < fullPrice && fullPrice < 30000:
+    console.log(
+      `Даем скидку 5%. Итоговая сумма: ${
+        servicePercentPrice - (servicePercentPrice * 5) / 100
+      }рублей`
+    );
+    break;
+  case 15000 >= fullPrice:
+    console.log(
+      `Скидка не предусмотрена. Итоговая сумма: ${servicePercentPrice}рублей`
+    );
+    break;
+  default:
+    console.log("Что то пошло не так");
+}
