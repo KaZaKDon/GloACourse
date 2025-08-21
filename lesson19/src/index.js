391c149069e1801d88234079cdbab5f6c71aa527
Kazak <strannik19742@mail.ru>
2025-08-21 21:19:41 +0300

    lesson19

---
+++ lesson19/src/index.js
@@ -0,0 +1,6 @@
+import timer from "./modules/timer";
+import two from "./modules/two";
+
+
+timer('20 september 2025')
+two()
---
+++ lesson19/src/modules/two.js
@@ -0,0 +1,5 @@
+const two = () => {
+    console.log("two12");
+}
+
+export default two
