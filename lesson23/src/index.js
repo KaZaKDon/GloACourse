4613f307a2f04256a7ccb9f3e7f89c5c3bc4ae88
Kazak <strannik19742@mail.ru>
2025-09-02 21:08:44 +0300

    lesson23

---
+++ lesson23/src/index.js
@@ -0,0 +1,16 @@
+import timer from "./modules/timer.js";
+import menu from "./modules/menu.js";
+import modal from "./modules/modal.js";
+import valid from "./modules/valid.js";
+//import animation from "./modules/animation.js"
+import tabs from "./modules/tabs.js";
+import slider from "./modules/slider.js";
+
+
+timer('20 september 2025')
+slider()
+menu()
+modal()
+valid()
+//animation()
+tabs()
